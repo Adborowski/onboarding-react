82aef892850c4cd3d35383c4864c114e124b52c1
Adam Borowski <adborowski@gmail.com>
2023-09-15 14:16:56 +0200

    added basic question and answer display

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
 import styles from './App.module.scss'
 import './globals.css'
 import { useState, useEffect } from 'react'
+import Question from './components/question/question'
+
+const getTestId = () => {
+   const testIds = ['conf_a', 'conf_b', 'conf_c']
+   const randomId = Math.floor(Math.random() * 10)
+   console.log(randomId)
+}
 
 const App = () => {
    const [questionsData, setQuestionsData] = useState<[]>()
+
    useEffect(() => {
       fetch(`safecap.json`)
          .then((r) => r.json())
          .then((data) => {
             if (data) {
-               console.log('Q', data)
-               setQuestionsData(questionsData)
+               setQuestionsData(data)
             }
          })
    }, [])
+
+   useEffect(() => {
+      console.log('questionsData', questionsData)
+   }, [questionsData])
 
    return (
       <>
          <h1>markets.com</h1>
          {questionsData &&
-            questionsData.map(() => {
-               return <p>a</p>
+            questionsData.map((q: any) => {
+               return <Question question={q} testId={getTestId()} />
             })}
       </>
    )
--- src/components/question/question.tsx
+++ src/components/question/question.tsx
@@ -1,10 +1,34 @@
-interface QuestionProps {
+import styles from './question.module.scss'
+
+interface Question {
    group_id: string
    title: string
 }
 
-const Question = (props: QuestionProps) => {
-   console.log(props)
+const Question = (props: any) => {
+   const question: Question = props.question
+   const testId = { props }
+
+   const answers = [
+      { label: 'Answer A', value: 'a', inputType: 'radio' },
+      { label: 'Answer B', value: 'a', inputType: 'radio' },
+      { label: 'Answer C', value: 'a', inputType: 'radio' },
+      { label: 'Answer D', value: 'a', inputType: 'text' },
+   ]
+
+   return (
+      <div className={styles.question}>
+         aa
+         {answers.map((ans) => {
+            return (
+               <section className={styles.answer}>
+                  {ans.label}
+                  <input name={question.title} type={ans.inputType} />
+               </section>
+            )
+         })}
+      </div>
+   )
 }
 
 export default Question
